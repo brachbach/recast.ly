3f30738a2aba21cc19cb14036bdddb6179057e80
brachbach <brachbach@gmail.com>
2016-05-06 19:59:24 -0700

    Work on API requests

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,12 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  // options -> key, query, max
+
+  $.get('https://www.googleapis.com/youtube/v3/search', {
+    key: options.key,
+    q: options.query,
+    maxResults: options.max,
+    part: 'snippet'
+  }, result => { console.log(result.items); console.log(exampleVideoData); callback(result.items); }, 'json');
 };
 
 window.searchYouTube = searchYouTube;
